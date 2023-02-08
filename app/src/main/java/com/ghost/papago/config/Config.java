7b3f5d6fc2afcf8656421d2e2e3e42be685d3be3
GhostWing17 <ghostwing17@gmail.com>
2023-02-08 17:31:28 +0900

    파파고 번역 테스트

---
+++ app/src/main/java/com/ghost/papago/config/Config.java
@@ -0,0 +1,7 @@
+package com.ghost.papago.config;
+
+public class Config {
+    public static final String URL = "https://openapi.naver.com/v1/papago/n2mt";
+    public static final String CLIENT_ID = "YjjkhXzaW33jsPaZjAPZ";
+    public static final String CLIENT_SECRET = "4sRg3x2lhl";
+}
